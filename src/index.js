d281c9dfaf910eef172dd10c881f65bc047d32cb
Asajco <petergacj@gmail.com>
2023-09-14 14:47:41 +0200

    router change

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App'
 import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react'
 
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 const config = {
   initialColorMode: 'light',
@@ -15,9 +15,9 @@ root.render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ChakraProvider>
   </React.StrictMode>,
 )
